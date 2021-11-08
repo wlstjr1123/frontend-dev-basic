5724b955dc57ad2df267cfdd8fa0bf8d97a00d1b
wlstjr1123 <wlstjr1123@naver.com>
2021-11-08 11:27:16 +0900

    js basic #10

---
+++ javascript-practices/basics/ex10.js
@@ -0,0 +1,55 @@
+/**
+ * 배열
+ */
+
+//1. 내장객체(생성자 함수)
+console.log("====생성1:  내장객체(생성자 함수)============")
+
+var a1 = new Array(0, 2, 4, 6, 8);
+console.log(typeof(a1), a1 instanceof Array, a1.length);
+
+// 2. 리터럴
+console.log("====생성2:  리터럴============")
+var a4 = [function() {
+    console.log("Hello");
+}, 20, "JavaScript", true, {
+    name: "마이콜",
+    email: "michol@gmail.com"
+}];
+
+console.log(a4.length)
+for (var i = 0; i <a4.length; i++) {
+    console.log(a4[i]);
+}
+
+// Array VS Object
+console.log("====Array VS Object======================")
+a5 = [];
+a5[0] = 0;
+a5["1"] = 1;
+a5["name"] = "둘리"
+console.log(a5["0"], a5[1], a5.length);
+for(prop in a5) {
+    console.log(prop);
+}
+
+console.log("====Array VS Object======================")
+o5 = {};
+o5.length = 0;
+o5[0] = 0;
+o5["1"] = 1;
+o5["name"] = "둘리"
+console.log(o5["0"], o5[1], o5.length);
+for(prop in o5) {
+    console.log(prop);
+}
+
+console.log("===========")
+a5.forEach(function(e, index) {
+    console.log(index + ":" + e);
+});
+
+Array.prototype.forEach.call(o5, function(e, index)) {
+    console.log(index + ":" + e);
+}
+
