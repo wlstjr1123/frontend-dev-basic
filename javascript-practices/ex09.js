4f589f2fb1e09988acf21bf700d6987a2a71321b
wlstjr1123 <wlstjr1123@naver.com>
2021-11-08 10:23:34 +0900

    js commit

---
+++ javascript-practices/ex09.js
@@ -0,0 +1,29 @@
+/*
+    자바스크립트 객체4
+    prototype 기반의 상속과 오버라이딩(자바 스크립트 객체 지향 프로그래밍)
+*/ 
+
+var MyObject = function(name, age) {
+    this.name = name;
+    this.age = age;
+}
+
+MyObject.prototype.school = "BIPA";
+MyObject.prototype.course = "DOUZONE";
+MyObject.prototype.info = function() {
+    console.log(this.name + ":" + this.age + ":" + this.school + ":" + this.course);
+}
+
+// MyObject 객체 생성1
+var o1 = new MyObject("둘리", 10);
+o1.school = "DONGSOE"
+o1.info();
+
+// MyObject 객체 생성2
+var o2 = new MyObject("마이콜", 20);
+o2.info();
+
+// 오버라이딩
+o2.info = function {
+    console.log("비밀!");
+}
